a1affbb6190a2d7cf84b5fd91cce55c3a6e27c88
Jungu Guo <hey.jungu@gmail.com>
2024-07-08 07:29:47 -0400

    test

--- src/paths.ts
+++ src/paths.ts
@@ -7,6 +7,7 @@ export const paths = {
     customers: '/dashboard/customers',
     integrations: '/dashboard/integrations',
     settings: '/dashboard/settings',
+    reads: '/dashboard/reads',
   },
   errors: { notFound: '/errors/not-found' },
 } as const;
